a6433001e26bb036d029dae378ecd6f3ce8c7984
mohan2005mona <76114543+mohan2005mona@users.noreply.github.com>
2021-01-06 11:03:09 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,25 @@
+class GrandFather:
+		 print("all the assets")
+
+		 	def withdraw:
+		Parent.Familyname='okkaligas'
+		pass
+
+
+
+
+class Father(GrandFather):
+	Familyname='tttt'
+	print("cash")
+	def __init__(self,name):
+		print('customer initialization")
+	withdraw()
+
+
+class child(Father):
+	withdraw()
+	print("Assets and cash")
+	
+
+
+
