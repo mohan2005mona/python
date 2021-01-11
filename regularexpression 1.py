de1786be5626bdc7300ec199e29f29120e6bd053
mohan2005mona <76114543+mohan2005mona@users.noreply.github.com>
2021-01-11 17:25:33 +0530

    Update regularexpression 1.py

--- regularexpression 1.py
+++ regularexpression 1.py
@@ -7,3 +7,17 @@ else:
     print('Your email id is invalid')
 result=re.sub(r'[\w\.-]+@google.com','\2\1@gmail.com',email)
 print(result)
+
+
+import re
+str= '808-1234 #athis is my phone number         dafdfafdafaf'
+result=re.sub(r'-',' ',re.sub(r'#.*','',str))
+print(result)
+
+#program to replace the comments in the string and also the other charcters in the Phone number  i.e,. hyphen(-) and Dollar($)
+import re
+str= '808-12-$34 #athis is my phone number         dafdfafdafaf'
+result=re.sub(r'-',' ',re.sub(r'#.*','',str))
+print(result)
+result=re.sub(r'\D',' ',re.sub(r'#.*','',str))
+print(result)
